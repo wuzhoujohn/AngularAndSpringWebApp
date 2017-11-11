07b3a67af496c5f692ede9bbeb26b7ed35196460
wuzhoujohn <wuzhoujohn@hotmail.com>
2017-11-11 11:51:40 -0500

    Back end set up finished

---
+++ BookStoreBackEnd/src/main/java/com/johnwu/bookstorebackend/BookStoreBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.johnwu.bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreBackEndApplication.class, args);
+	}
+}
